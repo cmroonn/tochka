a9086164730b1d6c5a095e241eb3c45dc2c983e4
cmroonn <fallgetell@gmail.com>
2020-05-25 17:44:23 +0500

    basic commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  var body = document.querySelector("body"); // Hamburger menu settings
+
+  {
+    var button = document.querySelector(".menu-btn");
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      button.classList.toggle("menu-btn-active");
+      body.classList.toggle("disabled");
+    });
+  }
+});
